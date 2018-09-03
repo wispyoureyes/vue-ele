ac609d58a7fcd8486be98ae2bfa7fd4d99a66917
wuyanqian <541989418@qq.com>
2018-09-03 18:17:37 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-     <div class="header">
-        I am header
-     </div>
+    <v-header></v-header>
      <div class="tab">
-        I am tab
+        <div class="tab-item">商品</div>
+        <div class="tab-item">评论</div>
+        <div class="tab-item">商家</div>
      </div>
      <div class="content">
        i am content
@@ -13,18 +13,14 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
+import header from './components/header/header.vue';
+export default{
+  components: {
+    'v-header': header
+  }
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    我是header
+  </div>
+</template>
+
+<script>
+export default{};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+body
+  background:red
+
+#app
+  .tab
+    display:flex
+    width:100%
+</style>
